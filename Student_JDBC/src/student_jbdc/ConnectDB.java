cdc0846efc93a78ef1ef8489e10d82b462f67ab6
hyunhub <nuyhos_ambi@naver.com>
2024-11-19 16:36:12 +0900

    [append]Student_JDBC. 기타 수정.

--- Student_JDBC/src/student_jbdc/ConnectDB.java
+++ Student_JDBC/src/student_jbdc/ConnectDB.java
@@ -16,48 +16,16 @@ public class ConnectDB
 		ResultSet rs = null;
 		ArrayList <Employees> employeesList = new ArrayList <Employees>();
 		
-		// 1. JDBC Driver Load
-		try
-		{
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			System.out.println("1. 드라이버 적재 성공.");
-		} catch (ClassNotFoundException e)
-		{
-			System.out.println("1. 드라이버 적재 실패." + e.toString());
-		}
-		
-		// 2. Connection
-		try
-		{																			/* 사용자명, 비밀번호. */
-			con = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521/orcl", "hr", "hr");
-			System.out.println("2. 오라클 접속 성공.");
-		} catch (SQLException e)
-		{
-			System.out.println("2. 오라클 접속 실패." + e.toString());
-		}
+		// 1번(JDBC Driver Load), 2번(Connection)을 가져온다.
+		con = DBConnection.dbCon();
 
 		// 3. Statement(Query : C R U D)
-		try
-		{
-			stmt = con.createStatement();
-			System.out.println("3. Statement 객체 생성 성공.");
-		} catch (SQLException e)
-		{
-			System.out.println("3. Statement 객체 생성 실패.");
-		}
-		
 		// 4. Result Set
-		try {
-			rs = stmt.executeQuery("SELECT * FROM EMPLOYEES");
-			System.out.println("4. Result Set 객체 생성 성공.");
-		} catch (SQLException e)
-		{
-			System.out.println("4. Result Set 객체 생성 실패.");
-		}
-		
 		// 5. (출력 진행.) 데이터 저장 진행.
 		try
 		{
+			stmt = con.createStatement();
+			rs = stmt.executeQuery("SELECT * FROM EMPLOYEES");
 			while (rs.next())
 			{
 				int employeeID = rs.getInt("EMPLOYEE_ID");
@@ -67,55 +35,19 @@ public class ConnectDB
 				Employees employees = new Employees(employeeID, firstName, salary);
 				employeesList.add(employees);
 			}
-		
-			
 		} catch (SQLException e)
 		{
-			System.out.println("5. Result Set 출력 실패.");
+			System.out.println("데이터베이스 실행문 에러." + e.toString());
 		}
-		
-		// 데이터 출력.
+
+		// 6. 데이터 출력.
 		employeesListPrint(employeesList);
 		
-		
-		// 6. Close
-		if (con != null)
-		{
-			try
-			{
-				con.close();
-				System.out.println("5. 오라클 con close 성공.");
-			} catch (SQLException e)
-			{
-				System.out.println("5. 오라클 con close 실패." + e.toString());
-			}
-		}
-		
-		if (stmt != null)
-		{
-			try
-			{
-				stmt.close();
-				System.out.println("6. 오라클 stmt close 성공.");
-			} catch (SQLException e)
-			{
-				System.out.println("6. stmt close 실패." + e.toString());
-			}
-		}
-		
-		if (rs != null)
-		{
-			try
-			{
-				rs.close();
-				System.out.println("6. 오라클 rs close 성공.");
-			} catch (SQLException e)
-			{
-				System.out.println("6. rs close 실패." + e.toString());
-			}
-		}
+		// 7. Close
+		DBConnection.dbClose(con, stmt, rs);
 	}
-
+	
+	// 6. 테이블 내용 출력.
 	private static void employeesListPrint(ArrayList <Employees> employeesList)
 	{
 		for (Employees e : employeesList)
