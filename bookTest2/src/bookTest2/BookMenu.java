98d93392262ad1fb2e6dae281b467b373d5a9764
hyunhub <nuyhos_ambi@naver.com>
2024-11-20 12:27:19 +0900

    [append]bookTest2.project PreparedStatement Practice.

---
+++ bookTest2/src/bookTest2/BookMenu.java
@@ -0,0 +1,11 @@
+package bookTest2;
+
+public interface BookMenu
+{
+	// 상수, 추상메소드, 디폴트메소드, 프라이빗메소드
+	/*public static final*/ int PRINT = 1
+						,INSERT = 2
+						,UPDATE = 3	
+						,DELETE = 4
+						,EXIT = 5;
+}
