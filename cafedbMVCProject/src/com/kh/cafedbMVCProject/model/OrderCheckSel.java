848aaa4d01679035dd7844c5e53ba267765c7e19
hyunhub <nuyhos_ambi@naver.com>
2024-11-27 01:26:44 +0900

    [append]cafedbMVCProject.project 주문번호로 개별 조회 기능 추가.

---
+++ cafedbMVCProject/src/com/kh/cafedbMVCProject/model/OrderCheckSel.java
@@ -0,0 +1,14 @@
+package com.kh.cafedbMVCProject.model;
+
+public class OrderCheckSel
+{
+	private int m_num;	// fk(MENU테이블로부터 NO참조.)
+	private String review;	
+	
+	private String drink;
+	private String snack;
+	private String dessert;
+	
+	
+
+}
